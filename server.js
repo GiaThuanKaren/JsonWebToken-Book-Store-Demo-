0da4e1c52267aa86a78d7945e8514e5c8e6b35ce
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-05-31 23:34:32 +0700

    Success send request and have expried token

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express();
 const port= process.env.PORT || 5000
 const book=[
@@ -20,7 +22,31 @@ const book=[
     },
 ]
 app.use(express.json());
-app.get('/books',(req,res)=>{
+
+//MiddleWare
+
+function AuthenToken(req,res,next){
+    const authorizationHeader=req.header('authorization');
+    const token= authorizationHeader.split(' ')[1];
+    if(!token) res.sendStatus(404);
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECREET,(err,data)=>{
+        if(err) res.sendStatus(403)
+        next();
+    })
+
+}
+
+
+app.post('/login',(req,res)=>{
+    ///Authorization
+    //Authentication
+    // {username :"test"}
+    const data = req.body;
+    const AccessToken=jwt.sign(data,process.env.ACCESS_TOKEN_SECREET,{expiresIn:'30s'});
+    res.json({AccessToken})
+})
+
+app.get('/books',AuthenToken,(req,res)=>{
     res.json({status:'Sucess',data : book} )
 })
 
